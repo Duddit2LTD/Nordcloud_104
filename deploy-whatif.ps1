988d7d0020eb9f524ca9fa11eade88e84177225a
fletcher-tom <64195084+fletcher-tom@users.noreply.github.com>
2021-09-24 09:49:26 +0100

    update key vault

--- deploy-whatif.ps1
+++ deploy-whatif.ps1
@@ -7,9 +7,12 @@ New-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -Templat
 
 New-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params
 
+Test-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params
+
 $rg = 'rg-team-5-sep-case-arm-we'
 $loc = 'northeurope'
 $template = 'G:\My Drive\Repos\Nordcloud_104\HeroVoting\Artifacts\LogAnalytics.json'
 $params = 'G:\My Drive\Repos\Nordcloud_104\HeroVoting\Artifacts\LogAnalytics.parameters.json'
 
 New-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params -WhatIf
+
