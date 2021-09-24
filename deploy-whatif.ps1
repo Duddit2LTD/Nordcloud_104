04b970722bc79bfec74d6be4098feb6874d22f36
fletcher-tom <64195084+fletcher-tom@users.noreply.github.com>
2021-09-24 09:20:08 +0100

    update

--- deploy-whatif.ps1
+++ deploy-whatif.ps1
@@ -6,3 +6,10 @@ $params = 'G:\My Drive\Repos\Nordcloud_104\HeroVoting\Deploy.parameters.json'
 New-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params -WhatIf
 
 New-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params
+
+$rg = 'rg-team-5-sep-case-arm-we'
+$loc = 'northeurope'
+$template = 'G:\My Drive\Repos\Nordcloud_104\HeroVoting\Artifacts\LogAnalytics.json'
+$params = 'G:\My Drive\Repos\Nordcloud_104\HeroVoting\Artifacts\LogAnalytics.parameters.json'
+
+New-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params -WhatIf
