b66d83a4bba71bd20a8b458843dbc38c22e51fa5
duddit2 <58218990+duddit2@users.noreply.github.com>
2021-09-24 14:18:14 +0100

    lowercase issue

--- deploy-whatif.ps1
+++ deploy-whatif.ps1
@@ -1,7 +1,7 @@
 $rg = 'rg-team-5-sep-case-arm-we'
 $loc = 'northeurope'
-$template = 'G:\My Drive\Repos\Nordcloud_104\HeroVoting\Deploy.json'
-$params = 'G:\My Drive\Repos\Nordcloud_104\HeroVoting\Deploy.parameters.json'
+$template = '.\HeroVoting\Deploy.json'
+$params = '.\HeroVoting\Deploy.parameters.json'
 
 New-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params
 
@@ -11,15 +11,15 @@ Test-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -Templa
 
 $rg = 'rg-team-5-sep-case-arm-we'
 $loc = 'northeurope'
-$template = 'G:\My Drive\Repos\Nordcloud_104\HeroVoting\Artifacts\LogAnalytics.json'
-$params = 'G:\My Drive\Repos\Nordcloud_104\HeroVoting\Artifacts\LogAnalytics.parameters.json'
+$template = '.\HeroVoting\Artifacts\LogAnalytics.json'
+$params = '.\HeroVoting\Artifacts\LogAnalytics.parameters.json'
 
 New-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params -WhatIf
 
 $rg = 'rg-team-5-sep-case-arm-we'
 $loc = 'northeurope'
-$template = 'G:\My Drive\Repos\Nordcloud_104\HeroVoting\Artifacts\Bastion.json'
-$params = 'G:\My Drive\Repos\Nordcloud_104\HeroVoting\Artifacts\Bastion.parameters.json'
+$template = '.\HeroVoting\Artifacts\Bastion.json'
+$params = '.\HeroVoting\Artifacts\Bastion.parameters.json'
 
 New-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params -WhatIf
 
