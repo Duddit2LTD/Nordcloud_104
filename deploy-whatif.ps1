367a52fb4b958b1862b9822c83cec20a8d49d5ed
duddit2 <58218990+duddit2@users.noreply.github.com>
2021-09-28 11:17:55 +0100

    VMSS

--- deploy-whatif.ps1
+++ deploy-whatif.ps1
@@ -3,23 +3,14 @@ $loc = 'northeurope'
 $template = '.\HeroVoting\Deploy.json'
 $params = '.\HeroVoting\Deploy.parameters.json'
 
-New-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params
+New-AzResourceGroupDeployment -name MainDeployment -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params
 
-New-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params -WhatIf
+New-AzResourceGroupDeployment -name MainDeployment -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params -WhatIf
 
-Test-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params
 
-$rg = 'rg-team-5-sep-case-arm-we'
-$loc = 'northeurope'
-$template = '.\HeroVoting\Artifacts\LogAnalytics.json'
-$params = '.\HeroVoting\Artifacts\LogAnalytics.parameters.json'
 
-New-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params -WhatIf
 
-$rg = 'rg-team-5-sep-case-arm-we'
-$loc = 'northeurope'
-$template = '.\HeroVoting\Artifacts\Bastion.json'
-$params = '.\HeroVoting\Artifacts\Bastion.parameters.json'
 
-New-AzResourceGroupDeployment -name loganalytics -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params -WhatIf
+
+
 
