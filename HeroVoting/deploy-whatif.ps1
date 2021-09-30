e66b0f6593151db55d237a27c662c119ec9cb811
duddit2 <58218990+duddit2@users.noreply.github.com>
2021-09-30 07:34:09 +0100

    script tidy

--- HeroVoting/deploy-whatif.ps1
+++ HeroVoting/deploy-whatif.ps1
@@ -1,15 +1,29 @@
+#Use these to set default variables for main deployment
 $rg = 'rg-team-5-sep-case-arm-we'
 $loc = 'northeurope'
 $template = '.\Deploy.json'
 $params = '.\Deploy.parameters.json'
 
-New-AzResourceGroupDeployment -name MainDeployment -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params
 
-New-AzResourceGroupDeployment -name MainDeployment -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $params -WhatIf
+#Deployment WHAT IF
+New-AzResourceGroupDeployment `
+    -name MainDeployment`
+    -ResourceGroupName $rg `
+    -TemplateFile $template `
+    -TemplateParameterFile $params
+    -WhatIf
+
+#Deployment
+New-AzResourceGroupDeployment `
+    -name MainDeployment `
+    -ResourceGroupName $rg `
+    -TemplateFile $template `
+    -TemplateParameterFile $params `
+    
 
 
 
-
+#use these to set temp file path variables
 $template = '.\Artifacts\Application.json'
 $params = '.\Artifacts\Application.parameters.json'
 
